0bc2c62f89ccfc6ed2b0b25ddef7aceacf96f5a5
garsin <garsinap@gmail.com>
2018-03-04 22:20:16 +0530

    corrected code

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 int main(){ printf("Helloworld!\n");
 printf("This must be a monolithic design\n");
 microkernel_sendmsg("is more portable");
