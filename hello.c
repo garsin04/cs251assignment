b3d4d423fa9badb6d849d5e3c3a1c30364c10941
garsin <garsinap@gmail.com>
2018-03-04 21:52:57 +0530

    change to c file by user2

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 
 int main(){ printf("Helloworld!\n");
 printf("This must be a monolithic design\n");
@@ -9,5 +10,8 @@ return 0;
 }
 
 void microkernel_sendmsg(char *a){ printf("microkernel: %s\n", a);
+}
+
+void microkernel_getmsg(char *b){ //TODO: getmsg feature
 }
 
